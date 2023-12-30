8b032c0e3bf18d6e700bafefed21f3c4a235b0b5
Nathaniel <130931122+nathanielhowe97@users.noreply.github.com>
2023-12-29 19:41:55 -0600

    bug fixes - js files
    - fixed issue with overflow on nav menu (maybe)

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,15 +1,10 @@
+let wrapper = document.querySelector('.body')
 const navLinks = document.querySelector('.nav-links')
 let visible = document.querySelector('.nav-links--visible')
 let menu = document.querySelector('.hamburger')
 
 menu.addEventListener('click', function(){
     navLinks.classList.toggle("nav-links--visible")
+    wrapper.classList.toggle("no-overflow")
 
-    if(!visible){
-        navLinks.style.animation = "fade .1s linear"
-    }
-
-    if (visible){
-        navLinks.style.animation = "fadeOut .1s linear"
-    }
 })
